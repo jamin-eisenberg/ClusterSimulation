6ea87ce324358d44a875b8e1256778513f0149ca
Jamin <tiedyeguy18@gmail.com>
2023-11-20 20:09:37 -0500

    reading distance

--- cluster_simulation.py
+++ cluster_simulation.py
@@ -1,11 +1,53 @@
 import pygame
 import sys
+import RPi.GPIO as GPIO
+import time
+
+def read_distance():
+    try:
+      GPIO.setmode(GPIO.BOARD)
+
+      PIN_TRIGGER = 16
+      PIN_ECHO = 18
+
+      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+      GPIO.setup(PIN_ECHO, GPIO.IN)
+
+      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+      time.sleep(0.2)
+
+      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+
+      time.sleep(0.00001)
+
+      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+      while GPIO.input(PIN_ECHO)==0:
+            pulse_start_time = time.time()
+      while GPIO.input(PIN_ECHO)==1:
+            pulse_end_time = time.time()
+
+      pulse_duration = pulse_end_time - pulse_start_time
+      distance = round(pulse_duration * 17150, 2)
+
+    finally:
+      GPIO.cleanup()
 
 pygame.init()
+pygame.font.init()
+
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
+
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+width, height = screen.get_size()
+
 pygame.display.set_caption("Hello World")
 while True:
     screen.fill((255, 0, 0))
+    text_surface = my_font.render(f'Distance: {dist}', False, (0, 0, 0))
+    screen.blit(text_surface, (width / 2, height / 2))
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
