dd0aaab91760bc6db511916360ddcaa12b2c99ea
Jamin <tiedyeguy18@gmail.com>
2023-11-20 20:19:57 -0500

    fixed distance edge case

--- cluster_simulation.py
+++ cluster_simulation.py
@@ -23,8 +23,11 @@ def read_distance():
 
       GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
+      pulse_start_time = time.time()
       while GPIO.input(PIN_ECHO)==0:
             pulse_start_time = time.time()
+
+      pulse_end_time = time.time()
       while GPIO.input(PIN_ECHO)==1:
             pulse_end_time = time.time()
 
